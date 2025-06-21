5f303e2d50ece6f2d9a82ebba6bd6539e1387206
Enrico <enrico.melis.casa@gmail.com>
2025-06-21 12:32:10 +0200

    removed unused import

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,5 +1,4 @@
 import { AuthButton } from "@/components/auth-button";
-import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
